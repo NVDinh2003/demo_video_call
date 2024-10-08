1cf0e3df4bf3685182fc92e069aef90e24da5824
Nguyen Van Dinh <nvdinh2003@gmail.com>
2024-10-08 15:11:47 +0700

    first commit

---
+++ src/main/java/com/nvd/DemoVideoCallApplication.java
@@ -0,0 +1,13 @@
+package com.nvd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoVideoCallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoVideoCallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nvd/DemoVideoCallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nvd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoVideoCallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
